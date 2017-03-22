cda85fe95cc57922979efef6c60229c29e39a75a
RathiJ <rathi.jeena@gmail.com>
2017-03-22 13:09:50 +0530

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,347 @@
+var bhaktamar ={"heading":"Bhaktamar Stotra",
+"content":["1. Bhaktamara pranata mauli maniprabhanam",
+ "uddyotakam dalita papa tamo vitanam",
+ "Samyak pranamya jinapadayugam yugada-",
+ "valambanam bhavajale patatam jananam",
+ " <BR> ",
+" 2. Yah samstutah sakala vangmaya tatva bodhat",
+"udbhuta buddhi patubhih suraloka nathaih.",
+"Stotrairjagattritaya chittaharairudaraih",
+"stoshye kilahamapi tam prathamam jinendram.",
+ " <BR> ",
+" 3. Buddhya vinaapi vibudharchita padapitha",
+"stotum samudyata matirvigata trapoaham.",
+"Balam vihaya jala samsthitam indu bimbam",
+" anyah ka ichchhati janah sahasa grahitum.",
+" <BR> ",
+" 4. Vaktum gunan gunasamudra shashankakantan",
+" kaste kshamah suraguru pratimoapi buddhya.",
+" Kalpanta kala pavanoddhata nakra chakram",
+" ko va taritum alam ambunidhim bhujabhyam.",
+" <BR> ",
+" 5. soaham tathapitava bhaktivashanmunisha",
+" kartum stavam vigata shaktirapi pravrittah.",
+" Prityatma viryam avicharyamrigimrigendram",
+" nabhyeti kim nijashishoh paripalanartham.",
+" <BR> ",
+" 6. Alpashrutam shrutavatam parihasadhama",
+" tvadbhakti-reva mukhari kurute balanmam.",
+" Yat kokilah kila madhau madhuram virauti ",
+" tachchamra charu kalika nikaraika-hetuh.",
+" <BR> ",
+" 7. Tvat samstavena bhavasantati sannibaddham",
+" papam kshanat kshayamupaiti sharirabhajam.",
+" Akranta loka malinilama sheshamashu",
+" suryamshu bhinnamiva sharvaram andhakaram.",
+" <BR> ",
+" 8. Matveti nath! tava samstavanam mayedam",
+" arabhyate tanudhiyapi tava prabhavat.",
+" Cheto harishyati satam nalinidaleshu",
+" muktaphal dyutim upaiti nanudabinduh.",
+" <BR> ",
+" 9. Astam tava stavanam asta samasta dosham",
+" tvat samkathapijagatam duritani hanti.",
+" Dure sahasrakiranah kurute prabhaiva",
+" padmakareshu jalajani vikasha bhanjt.",
+" <BR> ",
+" 10. Natyadbhutam bhuvana bhushana! bhutanatha",
+" bhutairgunairbhuvi bhavantam abhishtuvantah.",
+" Tulya bhavanti bhavato nanu tena kim va",
+" Bhutyashritam ya iha natmasamam karoti",
+" <BR> ",
+" 11. Drishtva bhavantam animesha vilokaniyam",
+" nanyatra tosham uypayatijanasya chakshuh.",
+" Pitvapayah shashikara dyuti dugdha sindhoh",
+" Ksharam jalam jalanidhe rasitum ka ichchhet?",
+" <BR> ",
+" 12. Yaih shantaraga ruchibhih paramanubhistvam",
+" nirmapitastribhuvanaika lalamabhuta.",
+" Tavanta eva khalu teapyanavah prithivyam",
+" yat te samanam aparam na hi rupam asti.",
+" <BR> ",
+" 13. Vaktram kva te sura naroraga netra hari",
+" nihshesha nirjita jagat tritayopamanam.",
+" Bimbam kalanka malinam kva nishakarasya",
+" Yadvasare bhavati pandu palasha kalpam",
+" <BR> ",
+" 14. Sampurna mandala shashanka kala kalapa",
+" Shubhra gunastribhuvanam tava langhayanti.",
+" Ye samshritastrijagadishvara! Nathamekam",
+" kastan nivarayati samcharato yatheshtam.",
+" <BR> ",
+" 15. Chitram kimatrayadi te tridashanganabhir",
+" nitam managapi mano na vikara margam.",
+" Kalpanta kala maruta chalitachalena",
+" Kim mandaradri shikharam chalitam kadachit?",
+" <BR> ",
+" 16. Nirdhuma varttirapavarjita taila purah",
+" Kritsnam jagat trayamidam prakati karoshi.",
+" Gamyo najatu marutam chalitachalanam",
+" dipoaparastvamasi natha ! jagatprakashah.",
+" <BR> ",
+" 17. Nastam kadachidupayast na rahugamyah",
+" spashtikaroshi sahasa yugapajjaganti.",
+" Nambhodharodara niruddha maha prabhavah",
+" Suryatishayi mahimasi munindra ! loke.",
+" <BR> ",
+" 18. Nityodayam dalitamoha mahandhakaram",
+" gamyam na, rahuvadanasya na varidanam.",
+" Vibhrajate tava mukhabjamanalpakanti",
+" vidyotayajjagadapurvashashanka bimbam.",
+" <BR> ",
+" 19. Kim sharvarishu shashinanhi vivasvata va",
+" yushman mukhendu daliteshu tamassu natha!",
+" Nishpanna shalivana shalini jivaloke",
+" karyam kiyajjaladharairjalabhara namraih.",
+" <BR> ",
+" 20. Jnanamyatha tvayi vibhati kritavakasham",
+" naivam tatha Hari Haradishu nayakeshu.",
+" Teiah sfuran manishu yati yatha mcihattvam",
+" naivam tu kachashakale kiranakuleapi.",
+" <BR> ",
+" 21. Manye varam Hari Haradaya eva drishta",
+" Drishteshu yeshu hridayam tvayi to-shameti.",
+" Kim vikshitena bhavata bhuviyena nanyah",
+" kashchinmano harati natha! bhavantareapi.",
+" <BR> ",
+" 22. Strinam shatani shatasho janayanti putran",
+" nanya sutam tvadupamam janani prasuta.",
+" Sarva disho dadhati bhani sahasrarashmim",
+" Prachyeva digianayati sphuradamshujalam.",
+" <BR> ",
+" 23. Tvamamanantimunayah paramam pumamsham",
+" adityavarnam amalam tamasahpurastat.",
+" Tvameva samyagupalabhyajayanti mrityum",
+" nanyah shivah shivapadasya munindra ! panthah.",
+" <BR> ",
+" 24. Tvamavyayam vibhumachintyq masankhyamadyam",
+" Brahmanamishvaramanantamanangaketum.",
+" Yogishvaram viditayogamanekamekam",
+" Jnanasvarupamamalam pravadanti santah.",
+" <BR> ",
+" 25. Buddhastvameva vibudharchita buddhi bodhat",
+" tvam Shankaroasi bhuvanatraya shankaratvat.",
+" Dhatasi dhira! shivamarga vidhervidhanat",
+" Vyaktam tvameva Bhagavan! purushottamoasi.",
+" <BR> ",
+" 26. Tubhyam namastribhuvanartiharaya natha",
+" Tubhyam namah kshititalamala bhushanaya.",
+" Tubhyam namastrijagatah parameshvaraya",
+" Tubhyam namo jina! Bhavodadhi shoshanaya.",
+" <BR> ",
+" 27. Ko vismayoatra yadi nama gunairasheshaih",
+" tvam sainshrito niravakashataya munisha.",
+" Doshairupatta vividhashraya jatagarvalh",
+" svapnantareapi na kadachidapikshitosil.",
+" <BR> ",
+" 28. Uchchairashokatarusamshrita munmayukham",
+" abhati rupamamalam bhavato nitantam.",
+" Spashtollasat kiranamasta tamo vitanam",
+" bimbam raveriva payodhara parshvavarti",
+" <BR> ",
+" 29. Simhasane mani mayukha shikha vichitre",
+" vibhrajate tava vapuh kanakavadatam.",
+" Bimbam viyadvilasadamshulata vitanam",
+" tungodayadri shirasiva sahasrarashmeh.",
+" <BR> ",
+" 30. Kundavadata chala chamara charushobham",
+" vibhrajate tava vapuh kaladhautakantam.",
+" udyachchhashanka shuchi nirjhara varidharam",
+" uchchaistatam suragireriva shatakaumbham",
+" <BR> ",
+" 31. Chhatratrayam tava vibhati shashanka kantam",
+" uchchaih sthitam sthagita bhanukara pratapam.",
+" Muktaphala prakarajala vivraddhashobham",
+" prakhyapayat trijagatah parameshvaratvam.",
+" <BR> ",
+" 32. Gambhira tara rava purita digvibhagah",
+" trailokya loka shubha sangama bhuti dakshah.",
+" Saddharmaraja jaya ghoshana ghoshakah san",
+" khe dundubhirdhvanati te yashasah pravadi.",
+" <BR> ",
+" 33. Mandara sundara nameru suparijata",
+" santanakadi kusumotkara vrishti ruddha.",
+" Gandhoda bindu shubha manda marutprapata",
+" Divya divah patati te vachasam tatirva.",
+" <BR> ",
+" 34. Shumbhat prabha valaya bhuri vibha vibhoste",
+" lokatraya dyutimatam dyutimakshipanti.",
+" Prodyad divakara nirantara bhuri samkhya",
+" diptyajayatyapi nishamapi somasaumyam.",
+" <BR> ",
+" 35. Svargapavargagama marga vimarganeshtah",
+" saddharmatatva kathanaikapatustrilokyah.",
+" Divyadhvanirbhavati te vishadartha sarva",
+" Bhasha svabhava parinama gunaih prayojyah.",
+" <BR> ",
+" 36. Unnidra hema nava pankaja puniakanti",
+" paryullasannakha mayukha shikhabhiramau.",
+" Padau padani tava yatra Jinendra dhattah",
+" padmani tatra vibudhahparikalpayanti.",
+" <BR> ",
+" 37. Itthamyatha tava vibhutirabhujjinendra!",
+" dharmopadeshanavidhau na tathaparasya.",
+" Yadrikprabha dinakritahprahatandhakara",
+" tadrik kuto grahaganasya vikashinoapi.",
+" <BR> ",
+" 38. Schyotanmadavila vilolaka pola mula",
+" mattabhramad bhramara nada vivriddhakopam.",
+" Airavata bhami bhamuddhatam apatantam",
+" dristva bhayam bhavati no bhavadashritanam.", 
+" <BR> ",
+" 39. Bhinnebha kumbhgaladujjvala shonitakta",
+" muktaphalaprakara bhushita bhumibhagah.",
+" Baddhakramah kramagatam harinadhipoapi",
+" nakramatikramayugachalasamshritamte.",
+" <BR> ",
+" 40. Kalpanta kalapavanoddhata vahnikalpam",
+" davanalamjvalitam ujjvalam utsphulingam.",
+" Visvamjighatsumivasammukhamapatantam",
+" tvannamakirttanajalam shamayatyashesham.",
+" <BR> ",
+" 41. Raktekshanam samada kokila kantha nilam",
+" krodhoddhatamphaninam utphanamapatantam.",
+" Akramati kramayugena nirastashankah",
+" tvannamanagadamanihridiyasyapumsah.",
+" <BR> ",
+" 42. Valgat turangagajagarjita bhima nadam",
+" ajau balam balavatamapi bhupatinam.",
+" Udyaddivakaramayukhashikha aviddham",
+" tvatkirtanat tama ivashu bhidamupaiti.",
+" <BR> ",
+" 43. Kuntagra bhinnagaja shonita varivaha",
+" vegavatara taranaturayodha bhime.",
+" Yuddhejayam vijita durjayajeyapakshah",
+" tvatpadapankaja vanashrayino labhante.",
+" <BR> ",
+" 44. Ambhonidhau kshubhita bhishana nakrachakra",
+" pathinapitha bhayadolvana vadavagnau.",
+" Rangat taranga shikhara sthitayanapatrah",
+" trasamvihayabhavatahsmaranadbrajanti.",
+" <BR> ",
+" 45. Udbhuta bhishanajalodara bhara bhugnah",
+" shochyam dasha mupagatah chyutajivitashah.",
+" Tvatpadapa,nkaja rajoamrita digdhadeha",
+" martya bhavanti makaradhvaja tulya rupah.",
+" <BR> ",
+" 46. Apada kanthamuru shrinkhala vestitanga",
+" gadham brihannigadakoti nighrishtajanghah.",
+" Tvannama mantramanisham manujah smarantah",
+" sadyah svayam vigata bandha bhayabhavanti.",
+" <BR> ",
+" 47. Mattadvipendra mrigaraja davanalahi",
+" samgrama varidhi mahodara bandhanottham.",
+" Tasyashu nashamupayati bhayam bhlyeua",
+" yastavakam stavamimam matimanadhite.",
+" <BR> ",
+" 48. Stotrasrajam tava Jinendra! gunaih nibaddham",
+" bhaktya maya ruchira varna vichitrapusfhpam.",
+" Dhattejanoya iha kanthagatam ajasramtam",
+" Manatunga mavasha samupaiti Lakshmih."]
+,"audio":"bhakthamaram.mp3","audioId":"audioTag"};
+
+var panchamandra= {"heading":"Navkar Stotra",
+"content":[ "Om Namo Arihantanam","<BR>","Om Namo Siddhanam","<BR>","Om Namo Ayriyanam","<BR>","Om Namo Uvajjhayanam","<BR>","Om Namo Loe Savva-sahunam","<BR>","Eso Panch Namokaro","<BR>","Savva-pavappanasano","<BR>","Manglananch Savvesim","<BR>","Padhamam Havei Mangalam"]
+,"audio":"panchamanthram.mp3","audioId":"panchamandraaudio"};
+
+//var currentMenu = bhaktamar;
+//var currentList = bhaktamar.content;
+
+
+function readItem()
+{
+	//$('#audioTag').attr('src', 'sounds/'+ bhaktamar.audio)[0].play();
+	document.getElementById(currentMenu.audioId).play();
+	//document.getElementById('panchamandraaudio').play();
+	//$('#audioTag').play();
+	//$('#audioTag').attr('src', 'media/' + currentList[currentItemIndex].audio)[0].play();
+}
+
+function showItem()
+{
+	var contentjoin = (currentMenu.content).join("<BR/>");
+	$('#contentText').html(contentjoin);	
+}
+
+function stopplay()
+{
+	
+	$('#'+currentMenu.audioId).attr('src', 'sounds/'+currentMenu.audio).stop();
+	//document.getElementById(currentMenu.audioId).stop();
+	//$("#panchamandraaudio").attr('src','sounds/'+ panchamandra.audio).stop();
+}
+
+function pause()
+{
+	//$('#audioTag').attr('src', 'sounds/'+ bhaktamar.audio)[0].pause();
+	document.getElementById(currentMenu.audioId).pause();
+	//document.getElementById('panchamandraaudio').pause();
+}
+
+function rewind()
+{
+	(document.getElementById(currentMenu.audioId)).currentTime -= 30;
+	
+}
+
+function forward()
+{
+	(document.getElementById(currentMenu.audioId)).currentTime += 30;
+
+}
+function initialize(targetList)
+{
+	currentList = targetList;
+	currentItemIndex = 0;
+	$('h2').text(currentMenu.heading);
+	//stopplay();
+	$('#Navigation').show();
+	showItem();	
+	$('#Menu').hide();
+	$( ".Close" ).hide();
+	$( ".HamburgerMenu" ).show();
+}
+
+
+	
+
+
+$(document).ready(function()
+{
+	$('#bhaktamar').click(function(){currentMenu = bhaktamar; initialize(bhaktamar.content);});	
+	$('#panchamandra').click(function(){
+		currentMenu = panchamandra; 
+		initialize(panchamandra.content);
+	});
+	$('#audioRewindButton').click(function(){rewind(); });
+	$('#audioPlayButton').click(function(){	readItem();	});	
+	$('#audioPauseButton').click(function(){ pause(); });
+	$('#audioStopButton').click(function(){ stopplay(); });	
+	$('#audioForwardButton').click(function(){forward(); });
+		
+	
+});
+
+
+
+
+$(function(){
+$( ".Close" ).hide();
+$( "#Menu" ).hide();
+$( ".HamburgerMenu" ).click(function() {
+$('#Navigation').hide();
+$( "#Menu" ).slideToggle( "slow", function() {
+$( ".HamburgerMenu" ).hide();
+$( ".Close" ).show();
+});
+});
+});
+
+$(function(){
+$( ".Close" ).click(function() {
+$( "#Menu" ).slideToggle( "slow", function() {
+$( ".Close" ).hide();
+$( ".HamburgerMenu" ).show();
+});
+});
+});
